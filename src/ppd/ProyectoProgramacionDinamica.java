3a0d566b78cd30673cd5365df1186f02a94d9691
FAMILIA-PC\FAMILIA <https://github.com/RaikaSantiago/ProyectoFADA.git>
2018-06-04 18:49:46 -0500

    Renacer

---
+++ src/ppd/ProyectoProgramacionDinamica.java
@@ -0,0 +1,136 @@
+package ppd;
+
+import java.io.*;
+
+/*
+   UNIVERSIDAD DEL VALLE
+   FACULTAD DE INGENIERIA
+   MATERIA FADA, TEMA OPTIMIZACIÓN
+   PROYECTO JUSTIFICACIÓN DE TEXTO 
+
+   Planteamiento:
+ */
+public class ProyectoProgramacionDinamica {
+
+    public String Justificar(String words[], int width, int b) {
+
+        String Variable1 = "";
+        String Variable2 = "";
+
+        int cost[][] = new int[words.length][words.length];
+
+        //Acontinuación se representan dos bucles que me calcularan el costo 
+        //de poner palabras de i a j  en una linea
+        // Si las palabras  no encajan  en una linea, entonces ponemos un entero max_value
+        for (int i = 0; i < words.length; i++) {
+            cost[i][i] = width - words[i].length();
+            for (int j = i + 1; j < words.length; j++) {
+                cost[i][j] = cost[i][j - 1] - words[j].length() - 1;
+            }
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i; j < words.length; j++) {
+                if (cost[i][j] < 0) {
+                    cost[i][j] = Integer.MAX_VALUE;
+                    //System.out.print("prueba1 " + cost[i][j] + " |");
+                } else {
+                    cost[i][j] = (int) Math.pow(cost[i][j], 3);
+                    // System.out.print("prueba2 " + cost[i][j] + " |");
+                }
+            }
+        }
+
+        //minCost from i to len is found by trying
+        //j between i to len and checking which
+        //one has min value
+        int minCost[] = new int[words.length];
+        int result[] = new int[words.length];
+        for (int i = words.length - 1; i >= 0; i--) {
+            minCost[i] = cost[i][words.length - 1];
+            result[i] = words.length;
+            for (int j = words.length - 1; j > i; j--) {
+                if (cost[i][j - 1] == Integer.MAX_VALUE) {
+                    continue;
+                }
+                if (minCost[i] > minCost[j] + cost[i][j - 1]) {
+                    minCost[i] = minCost[j] + cost[i][j - 1];
+                    result[i] = j;
+                }
+            }
+        }
+        int n = 0;
+        int Auxiliar1;
+
+//        System.out.println("El Costo Minimo Es: " + minCost[0]);
+//        System.out.println("\n");
+        //finally put all words with new line added in 
+        //string buffer and print it.
+        StringBuilder builder = new StringBuilder();
+        do {
+            Auxiliar1 = result[n];
+            for (int i = n; i < Auxiliar1; i++) {
+                builder.append(words[i] + " ");
+
+            }
+
+            builder.append("\n");
+            //System.out.println(builder.append("1"));
+            n = Auxiliar1;
+        } while (Auxiliar1 < words.length);
+
+        if (b == 1) {
+            return Variable1 = builder.toString();
+        } else {
+            return Variable2 = Integer.toString(minCost[0]);
+        }
+
+    }
+
+    /*
+        El Metodo @LongitudMaximaString saca la longitud maxima en un arreglo 
+        como podria ser una palabra, esto con el fin de hacer una validación 
+        para el ancho del texto requerido y no genere conflicto con  el metodo 
+        justificar; @LongitudString sacara la palabra mas larga del arreglo y 
+        LNumbMaximo sacara  el tamaño en un dato numero  de la palabra mas larga
+        en el arreglo.
+     */
+    public int LongitudMaximaString(String[] array) {
+
+        int LongitudMax = 0;
+        String LongitudString = null;
+        for (String AuxMax : array) {
+            if (AuxMax.length() > LongitudMax) {
+                LongitudMax = AuxMax.length();
+                LongitudString = AuxMax;
+            }
+        }
+        //  System.out.println("Maximo Es: " + LongitudString);
+        int LNumbMaximo = LongitudString.length();
+        //  System.out.println("Maximo num Es: " + LMaximo);
+        return LNumbMaximo;
+    }
+
+    public String[] LeerArchivo(File direccion) {
+
+        String Texto = "";
+
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(direccion));
+            String Aux = "";
+            String bfRead;
+            while ((bfRead = bf.readLine()) != null) {
+                // haz ciclo, mientras bfRead tiene datos
+                Aux = Aux + bfRead; // guardado el texto del archivo 
+            }
+            Texto = Aux;
+            // variable donde queda guardado el texto
+        } catch (Exception e) {
+            System.err.println("No se encontro el archivo");
+        }
+        String[] arreglo = Texto.split(" ");
+
+        return arreglo;
+    }
+
+}
---
+++ src/ppd/Ventana.java
@@ -0,0 +1,231 @@
+package ppd;
+
+import java.io.*;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import ppd.ProyectoProgramacionDinamica;
+
+public class Ventana extends javax.swing.JFrame {
+
+    JFileChooser seleccionar = new JFileChooser();
+    File archivo;
+    FileInputStream entrada;
+    FileOutputStream salida;
+    File direccion;
+
+    public Ventana() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        TextoArea = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
+        Costo = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        InAncho = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Justificador De Texto");
+        setName("Justificador De Texto"); // NOI18N
+
+        TextoArea.setColumns(20);
+        TextoArea.setRows(5);
+        jScrollPane1.setViewportView(TextoArea);
+
+        jButton1.setText("Cargar Archivo");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        Costo.setEditable(false);
+        Costo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CostoActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("El Costo Minimo Es:");
+
+        InAncho.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                InAnchoActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Ancho Del Renglon:");
+
+        jButton2.setText("Limpiar Cuadros");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 326, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(100, 100, 100)
+                                .addComponent(jLabel2))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(InAncho, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(Costo, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(66, 66, 66)
+                        .addComponent(jButton1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(64, 64, 64)
+                        .addComponent(jButton2)))
+                .addContainerGap(54, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(77, 77, 77)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(InAncho, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(46, 46, 46)
+                        .addComponent(jButton1)
+                        .addGap(30, 30, 30)
+                        .addComponent(jLabel2)
+                        .addGap(11, 11, 11)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(Costo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(28, 28, 28)
+                        .addComponent(jButton2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 288, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(31, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        String[] Dato = null;
+        ProyectoProgramacionDinamica awl = new ProyectoProgramacionDinamica();
+
+        try {
+            int ancho = Integer.parseInt(InAncho.getText());
+            if (ancho <= 0 || InAncho.getText().length() == 0) {
+                JOptionPane.showMessageDialog(null, "Llene el campo del ancho con un valor numerico mayor a cero","Mensaje de Advertencia",JOptionPane.WARNING_MESSAGE);
+            } else {
+                if (seleccionar.showDialog(null, "Abrir") == JFileChooser.APPROVE_OPTION) {
+                    archivo = seleccionar.getSelectedFile();
+                    if (archivo.canRead()) {
+                        if (archivo.getName().endsWith("txt")) {
+
+                            Dato = awl.LeerArchivo(archivo);
+                            int Maximo = awl.LongitudMaximaString(Dato);
+                            if (ancho < Maximo) {
+                                String nl = System.getProperty("line.separator");
+                                JOptionPane.showMessageDialog(null, "Se encontro una palabra que excede el ancho del renglón solicitado."
+                                        + nl + "Por favor  aumente el ancho del renglón.","Mensaje Informativo",JOptionPane.INFORMATION_MESSAGE);
+                            } else {
+                                TextoArea.setText(awl.Justificar(Dato, ancho, 1));
+                                int gb = Integer.parseInt(awl.Justificar(Dato, ancho, 0));
+                                Costo.setText(String.valueOf(gb));
+                            }
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Archivo no compatible","Mensaje de Advertencia",JOptionPane.WARNING_MESSAGE);
+
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+
+            JOptionPane.showMessageDialog(null, "Llene el campo del ancho con un valor numerico.","Mensaje de Error",JOptionPane.ERROR_MESSAGE);
+        }
+
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void CostoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CostoActionPerformed
+
+    }//GEN-LAST:event_CostoActionPerformed
+
+    private void InAnchoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InAnchoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_InAnchoActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        Costo.setText("");
+        InAncho.setText("");
+        TextoArea.setText("");
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Ventana().setVisible(true);
+            }
+        });
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField Costo;
+    private javax.swing.JTextField InAncho;
+    private javax.swing.JTextArea TextoArea;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration//GEN-END:variables
+}
